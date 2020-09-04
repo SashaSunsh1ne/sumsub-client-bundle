f2035b481111ba79d64ae7b96f5f5644057efa07
Dmitry V. Alekseev <mail@alexeevdv.ru>
2020-09-04 18:51:37 +0300

    Fix bundle extension

--- src/SumSubClientBundle.php
+++ src/SumSubClientBundle.php
@@ -2,8 +2,17 @@
 
 namespace alexeevdv\Symfony\SumSub;
 
+use alexeevdv\Symfony\SumSub\DependencyInjection\SumSubClientExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class SumSubClientBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if ($this->extension === null) {
+            $this->extension = new SumSubClientExtension();
+        }
+
+        return $this->extension;
+    }
 }
